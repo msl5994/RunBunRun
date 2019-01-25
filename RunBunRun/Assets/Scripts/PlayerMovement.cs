620fe49b6623f734be9892132f0f6f716e8f5632
jrc3123 <jrc3123@rit.edu>
2019-01-25 16:26:21 -0500

    Implementing turning mechanics for sharp turn and smooth turn (left turns only, right turns not implemented yet)
    Read the summary, ya silly goose

--- RunBunRun/Assets/Scripts/PlayerMovement.cs
+++ RunBunRun/Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,10 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 1.0f;
     public float maxSpeed = 10.0f;
     public float angleIncrement = 45.0f;
+    public float turnTimer = 0.0f;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         // get a reference to the rigidbody
         rb = GetComponent<Rigidbody>();
@@ -23,19 +24,35 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKeyUp("a"))
+		if(Input.GetKeyUp(KeyCode.A))
         {
+            if (turnTimer < .2f)
+            {
+                transform.Rotate(new Vector3(0.0f, angleIncrement, 0.0f));
+            }
+            turnTimer = 0.0f;
             //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y + 15.0f, transform.rotation.z, 0.0f);
-            transform.rotation = Quaternion.Euler(0.0f, rb.rotation.y + angleIncrement, 0.0f);
+            //transform.rotation = Quaternion.Euler(0.0f, rb.rotation.y + angleIncrement, 0.0f);
+
+
             //rb.MoveRotation(Quaternion.Euler(0.0f, angleIncrement%360, 0.0f));
             //rb.rotation = new Quaternion(rb.rotation.x, rb.rotation.y + 45.0f, rb.rotation.z, 0.0f);
         }
-        else if(Input.GetKeyUp("d"))
+        else if(Input.GetKeyUp(KeyCode.D) && turnTimer < .3f)
         {
             //transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y - 15.0f, transform.rotation.z, 0.0f);
-            transform.rotation = Quaternion.Euler(0.0f, rb.rotation.y - angleIncrement, 0.0f);
+            //transform.rotation = Quaternion.Euler(0.0f, rb.rotation.y - angleIncrement, 0.0f);
+            transform.Rotate(new Vector3(0.0f, -angleIncrement, 0.0f));
             //rb.MoveRotation(Quaternion.Euler(0.0f, -angleIncrement%360, 0.0f));
             //rb.rotation = new Quaternion(rb.rotation.x, rb.rotation.y - 45.0f, rb.rotation.z, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            turnTimer += Time.deltaTime;
+            if (turnTimer > .2f)
+            {
+                transform.Rotate(new Vector3(0.0f, -30.0f * Time.deltaTime, 0.0f));
+            }
         }
     }
 
