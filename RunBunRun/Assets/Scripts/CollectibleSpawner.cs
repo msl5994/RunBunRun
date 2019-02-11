2a0899f07b1c39ada6ae8cc24d655b6736291879
Joseph Campanelli (RIT Student) <jrc3123@ad.rit.edu>
2019-02-11 18:31:50 -0500

    Revert "Collectible Spawner code"
    This reverts commit 8e87acb6a41d2b5dd139a92350ba9d0c4c6cce26.

--- RunBunRun/Assets/Scripts/CollectibleSpawner.cs
+++ RunBunRun/Assets/Scripts/CollectibleSpawner.cs
@@ -12,7 +12,6 @@ public class CollectibleSpawner : MonoBehaviour {
     public int numFeathersToSpawn = 5;
     public List<GameObject> carrotCollectibles;
     public List<GameObject> featherCollectibles;
-    private GameObject collectibleParent;
 
 	// Use this for initialization
 	void Start ()
@@ -20,7 +19,6 @@ public class CollectibleSpawner : MonoBehaviour {
         // instantiate the list
         carrotCollectibles = new List<GameObject>();
         featherCollectibles = new List<GameObject>();
-        collectibleParent = GameObject.Find("CollectibleParent");
 	}
 	
 	// Update is called once per frame
@@ -42,9 +40,6 @@ public class CollectibleSpawner : MonoBehaviour {
         // instantiate the collectible
         GameObject tempCollectible = Instantiate(carrotCollectiblePrefab, spawnPos, Quaternion.identity);
 
-        // set its parent
-        tempCollectible.transform.parent = collectibleParent.transform;
-
         // add it to a list of collectibles
         carrotCollectibles.Add(tempCollectible);
     }
@@ -61,9 +56,6 @@ public class CollectibleSpawner : MonoBehaviour {
 
         // instantiate the collectible
         GameObject tempCollectible = Instantiate(featherCollectiblePrefab, spawnPos, Quaternion.identity);
-
-        // set its parent
-        tempCollectible.transform.parent = collectibleParent.transform;
 
         // add it to a list of collectibles
         featherCollectibles.Add(tempCollectible);
